ac786cfbc90b0ed86689c6d5f5603ae3093d8a1b
David Rouleau <shabbypenguin@Davids-Mac-mini.local>
2021-07-12 10:46:05 -0400

    work on day 1 week 2

--- Week-2/Day-1/Activity-1.py
+++ Week-2/Day-1/Activity-1.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 
+class Table_info:
+	def __init__(self):
+		self.height = ""
+		self.width = ""
+		self.length = ""
+		self.material = ""
+		
+		
+table_1 = Table_info()
+table_1.height = "48"
+table_1.width = "38"
+table_1.length = "52"
+table_1.material = "glass"
+
+table_2 = Table_info()
+table_2.height = "50"
+table_2.width = "32"
+table_2.length = "46"
+table_2.material = "cherry"
