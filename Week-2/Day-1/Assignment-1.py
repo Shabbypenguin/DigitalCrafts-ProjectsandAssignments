d14886e6ad790e21e559660c89d40bc2b9d61d7d
David Rouleau <shabbypenguin@Davids-Mac-mini.local>
2021-07-12 16:15:06 -0400

    fix my earlier mistakes and add in item price, quantity, and title.

--- Week-2/Day-1/Assignment-1.py
+++ Week-2/Day-1/Assignment-1.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
 list_index = 0
-lists_of_lists = {}
-class List_Maker:
-	def __init__(self, location, name_of_list):
+lists_of_lists = []
+
+
+class Listmaker:
+	def __init__(self, store_name, location):
+		self.store_name = store_name
 		self.location = location
-		self.title = name_of_list
-		self.items = []
+		self.item_list = []
 		
-def view_list(list_index, lists_of_lists, i):
-	if list_index == 0:
-		print("No lists found")
-	else:
-		print("What list would you like to view?\n\n")
-		while i < list_index:
-			print(f"{i} {lists_of_lists[i].location}")
-			i+=1
-		list_choice_view = int(input())
-		if list_choice_view <= list_index:
-			print(lists_of_lists[list_choice_view].items)
-			return list_choice_view
+	def view_list(list_index, lists_of_lists, i):
+		if list_index == 0:
+			print("No lists found")
 		else:
-			print("Sorry that wasn't a valid choice, try again")
-			return False
+			print("What list would you like to view?\n\n")
+			while i < list_index:
+				print(f"{i} {lists_of_lists[i].store_name} located at {lists_of_lists[i].location}")
+				i+=1
+			list_choice_view = int(input())
+			if list_choice_view <= list_index:
+				for n in lists_of_lists[list_choice_view].item_list:
+					print(f"You need to buy {n.quantity} of {n.title} priced at {n.price} ")
+				return list_choice_view
+			else:
+				print("Sorry that wasn't a valid choice, try again")
+				return False
+			
+	def add_item(lists_of_lists, output, item):
+		lists_of_lists[output].item_list.append(item)
+		
+class GroceryItem:
+	def __init__(self):
+		self.title = ""
+		self.price = 0
+		self.quantity = 0
+		
 		
 while True:
 	i=0
@@ -30,25 +43,38 @@ while True:
 	print("2* Add items to a list")
 	print("3* View your current lists\n")
 	menu_input=input("Please enter your choice: \n")
+	
+	
 	if menu_input.upper() == "Q":
 		break
+	
+	
 	elif menu_input == "1":
-		print("\n\nWhere would you like to shop at?")
+		print("\n\nWhere would you like to shop at? ")
+		store_name = input("")
+		print("Where is this store located? ")
 		location = input("")
-		print("What would you like to call this list?")
-		list_tite = input("")
-		shopping_list = List_Maker(location, list_tite)
-		lists_of_lists[list_index] = shopping_list
-		print(lists_of_lists[list_index].location)
+		lists_of_lists.append(Listmaker(store_name, location))
+		print(lists_of_lists[list_index].store_name)
 		list_index+=1
+		
+		
 	elif menu_input == "2":
-		output = view_list(list_index, lists_of_lists, i)
+		output = Listmaker.view_list(list_index, lists_of_lists, i)
 		if output >= list_index:
 			break
+		item_to_add = GroceryItem()
 		print("What would you like to add to the list?")
-		add_items = input("")
-		lists_of_lists[output].items.append(add_items)
+		item_to_add.title = input("")
+		print("How much does it cost?")
+		item_to_add.price = float(input(""))
+		print(f"How much {item_to_add.title} do you want to buy?")
+		item_to_add.quantity = input("")
+		Listmaker.add_item(lists_of_lists, output, item_to_add)
+		
+		
 	elif menu_input == "3":
-		view_list(list_index, lists_of_lists, i)
+		Listmaker.view_list(list_index, lists_of_lists, i)
+		
 	else:
 		print("Sorry that wasn't a valid choice")
