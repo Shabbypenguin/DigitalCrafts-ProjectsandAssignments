501e98b67827648f25f0546df65366dfa3da1c59
David Rouleau <shabbypenguin@gmail.com>
2021-07-26 10:48:20 -0400

    week 4 activity 2

---
+++ Week-4/Day-1/number-sum.js
@@ -0,0 +1,11 @@
+
+function addNumbers(inputOne, inputTwo) {
+	let result = inputOne + inputTwo
+	return result
+}
+
+let inputOne = parseInt(window.prompt("Enter your first value: "), 10)
+let inputTwo = parseInt(window.prompt("Enter your second value: "), 10)
+sum = addNumbers(inputOne, inputTwo)
+
+console.log(sum)
