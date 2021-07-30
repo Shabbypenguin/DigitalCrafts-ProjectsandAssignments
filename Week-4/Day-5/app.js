c8fb231ec139a9ef8d4bf2506074cca38b2b8860
David Rouleau <shabbypenguin@gmail.com>
2021-07-30 12:08:45 -0400

    Continue week 4 day 5

---
+++ Week-4/Day-5/app.js
@@ -0,0 +1,11 @@
+const slideshowImages = ["images/poster1.png", "images/poster2.png", "images/poster3.png", "images/poster4.png", "images/poster5.png", ]
+let i = 0
+
+window.setInterval(function () {
+	let newPoster = document.getElementById("posterHome")
+	newPoster.src = slideshowImages[i]
+	i++
+	if (i==4){
+		i=0
+	}
+}, 2000)
