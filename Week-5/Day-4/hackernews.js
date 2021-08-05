6dcd7d9b3630f1915c083619b54d4969dd855057
David Rouleau <shabbypenguin@gmail.com>
2021-08-05 15:16:44 -0400

    Do day 4 of week 5

---
+++ Week-5/Day-4/hackernews.js
@@ -0,0 +1,29 @@
+const articleList = document.getElementById("articleList")
+
+async function getNewsStories() {
+	let response = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+	let newsArray = response.json()
+	return newsArray
+}
+
+function displayTheNews(articleInfo) {
+	let articledate = new Date(articleInfo.time * 1000)
+	let articleDetails = `
+		<a href="${articleInfo.url}"><h3>${articleInfo.title}</h3></a>
+		<h4>By: ${articleInfo.by} at ${articledate}<h4><br><br>`	
+	articleList.insertAdjacentHTML("beforeend", articleDetails)
+}
+
+async function getArticleInfo(newsArray) {
+	const newsArticle = newsArray.map(async function(article){
+		await fetch(`https://hacker-news.firebaseio.com/v0/item/${article}.json?print=pretty`)
+		.then(response => {return response.json()})
+		.then(result => {
+			displayTheNews(result)
+		})
+	})
+}
+
+getNewsStories().then(newsArray =>{
+	getArticleInfo(newsArray)
+})
